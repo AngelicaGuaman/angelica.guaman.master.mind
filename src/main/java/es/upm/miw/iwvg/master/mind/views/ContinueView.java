8f15859467d43d1e2b627857edf43ac889a6538b
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2018-10-01 02:27:20 +0200

    se crea vista

---
+++ src/main/java/es/upm/miw/iwvg/master/mind/views/ContinueView.java
@@ -0,0 +1,9 @@
+package es.upm.miw.iwvg.master.mind.views;
+
+import es.upm.miw.iwvg.master.mind.controllers.ContinueController;
+
+public class ContinueView {
+    void interact(ContinueController continueController) {
+        //continueController.resume(new YesNoDialog(Message.CONTINUE_PLAYING.getMessage()).read());
+    }
+}
